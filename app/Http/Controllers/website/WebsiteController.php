0936d0e799ab2b3c6e09134755c8f71e5289cc14
Saif Ibrahim <engineersaifibrahim@gmail.com>
2024-11-25 11:29:19 +0530

    cheking the code

--- app/Http/Controllers/website/WebsiteController.php
+++ app/Http/Controllers/website/WebsiteController.php
@@ -10,5 +10,8 @@ class WebsiteController extends Controller
     public function index() {
         echo"<h1>hello come to e-commerce website</h1>";
         echo"<h2>This is my E-commerce website</h2>";
+        echo"<h2>Anuj Singh</h2>";
+        echo"<h2>Saif ibrahim</h2>";
+        echo"<h2>This is my E-commerce website</h2>";
     }
 }
